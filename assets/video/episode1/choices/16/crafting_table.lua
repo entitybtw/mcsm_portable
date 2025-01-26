3f6d27aae5422635ba351f013b8243c282b00eaa
entitybtw <studenov.ivan020312@gmail.com>
2025-01-26 07:40:16 +0000

    fixes

--- assets/video/episode1/choices/16/crafting_table.lua
+++ assets/video/episode1/choices/16/crafting_table.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:53f99da325939125d1741ae05e60ce3e40e09a255efe2c43d3076b8dde3bd920
-size 1054
+oid sha256:c349a2b0113c85a1657181e36b225fa5e4438140dfd8280f21741ede461af20d
+size 885
