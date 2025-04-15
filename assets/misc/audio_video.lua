97b1f729e3ee8a89a21689de94ac867ff62ae3e5
entitybtw <studenov.ivan020312@gmail.com>
2025-04-15 18:20:23 +0000

    added hints, audio_video now more optimized, added new textures

--- assets/misc/audio_video.lua
+++ assets/misc/audio_video.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bada67cf6515f545aa0809040731ade6e0ed598b3b5a855a49bf2e5816cb1b76
-size 5347
+oid sha256:e520459c1a0e345144a077c6532ddcf1371defef5443f18360baa21582ac0128
+size 8035
