6bb82a79d9841fe8bb079abeb5e09f99ad75c238
entitybtw <studenov.ivan020312@gmail.com>
2025-12-04 13:51:30 +0300

    add crafting_table.pmp, crafting_table.lua, garden.lua, garden.pmp

---
+++ assets/video/episode5/choices/14/crafting_table.lua
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ee0b787640bdfefc2d956d165ec88587e0ccede835614e81824c55846efb0bf9
+size 1502
---
+++ assets/video/episode5/choices/14/garden.lua
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6772b6c2401aec8630c7d6c8322da698c531b59652ce72d14cec366c5ed3e7b9
+size 1486
