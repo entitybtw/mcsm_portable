7154141e4f318a7c28bd36ca1ef5b0b8f664cbd6
dntrnk <dantronilluminatormusic@gmail.com>
2025-12-22 15:16:17 +0700

    Update keep_it_down_milo.lua

--- assets/video/episode5/choices/21/milo/keep_it_down_milo.lua
+++ assets/video/episode5/choices/21/milo/keep_it_down_milo.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cda549ca09ec96c36d94f3001873fa32ed1997e06642feea3afa345c2c8eeb1f
-size 2879
+oid sha256:305e922c391cf0e8444487b05a4e53d1e8d6a872a57eda2603e77d4694395b68
+size 680
