97f9d243edf7bebfda749397f96b1b2c974459b3
dntrnk <dantronilluminatormusic@gmail.com>
2025-12-22 15:14:21 +0700

    Update petra_you_trust_him.lua

--- assets/video/episode5/choices/23/petra_you_trust_him.lua
+++ assets/video/episode5/choices/23/petra_you_trust_him.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec9b1831f64220c52e51314903bd2d130a74fe1e72cde280552c6593f9271e08
-size 2870
+oid sha256:56ab74ac0be093793a5f92ee55905f9138b11f6a4c111d09d8c9da5db264afd1
+size 686
