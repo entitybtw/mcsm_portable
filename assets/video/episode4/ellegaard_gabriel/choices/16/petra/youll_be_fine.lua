ee0aea9fe8e7f1f91c5245738e12e0a1978112f0
dntrnk <dantronilluminatormusic@gmail.com>
2025-12-26 17:02:13 +0700

    Update youll_be_fine.lua

--- assets/video/episode4/ellegaard_gabriel/choices/16/petra/youll_be_fine.lua
+++ assets/video/episode4/ellegaard_gabriel/choices/16/petra/youll_be_fine.lua
@@ -3,14 +3,14 @@ if crafting_table_used then petra_talk = true end
 PMP.setVolume(pmpvolume)
 local result = PMP.playEasy('assets/video/episode4/ellegaard_gabriel/choices/16/petra/youll_be_fine.pmp', buttons.r, true, 'assets/video/episode4/ellegaard_gabriel/choices/16/petra/you.srt', font, subssize, "#FFFFFF", "#000000/150", subs)
 if result == 1 then
-    nextscene = "./mainmenu.lua"
+    -- Go To Menu
     return 1
 end
-while choosing do
-if not in_interactive_zone then
-    petra = false
-    in_interactive_zone = true
-    choosing = false
-    break
-    end
-end
+-- while choosing do
+--     if not in_interactive_zone then
+--         petra = false
+--         in_interactive_zone = true
+--         choosing = false
+--         break
+--     end
+-- end
