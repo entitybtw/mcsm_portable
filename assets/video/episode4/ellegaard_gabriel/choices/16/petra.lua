bd5c4d9bae2700fcf6ed74911e37f11fdf252f70
dntrnk <dantronilluminatormusic@gmail.com>
2025-12-26 17:03:33 +0700

    Update petra.lua

--- assets/video/episode4/ellegaard_gabriel/choices/16/petra.lua
+++ assets/video/episode4/ellegaard_gabriel/choices/16/petra.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:81f80960f4c0a42b1ba9c41a4d4219701111615d39648b251d7499dfb080492e
-size 1888
+oid sha256:fabbbd3f8708b14c7f4c67661b09a1b1d21397c9c4fb77caccba93bbf25140ac
+size 1868
