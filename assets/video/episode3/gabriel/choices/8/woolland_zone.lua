43a7c9992e5b425a56c4a937d1ac0e72a26ff940
entitybtw <studenov.ivan020312@gmail.com>
2025-12-27 11:01:57 +0300

    woolland interactive zone fix

--- assets/video/episode3/gabriel/choices/8/woolland_zone.lua
+++ assets/video/episode3/gabriel/choices/8/woolland_zone.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7dae1963315538aae66dc8469c71508685433cdf741b7fc65b3ef5bf132d1535
-size 4886
+oid sha256:e20094626759ef19ffa45ed41103d5b8cd61a7a975d5fed8cac4c88e4633107b
+size 4988
--- assets/video/episode3/petra/choices/8/woolland_zone.lua
+++ assets/video/episode3/petra/choices/8/woolland_zone.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba189f8143d1172f7127b814785a7c4f8d9b1f303b6163fb6e6ab79a75e4876d
-size 4812
+oid sha256:a3ae2f1ca4967534f2ffaa292d4e79859c14777bc402fb436741be21e281e2f2
+size 4910
