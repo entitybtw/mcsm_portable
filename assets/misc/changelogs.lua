24fa4416d5b051ae7e6698723a7f8a17df89cab0
entitybtw <studenov.ivan020312@gmail.com>
2025-12-28 16:43:19 +0300

    add 1.7 changelog to changelogs.lua

--- assets/misc/changelogs.lua
+++ assets/misc/changelogs.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6533acbf92685619f70cca983f1efd122acc11189bdc704effd92b1b836be3da
-size 9744
+oid sha256:cbd8efa402b28f60bb7411a582394eeda2761a86198a444e3855a9d420afc382
+size 10702
