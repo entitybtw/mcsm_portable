720b97febe3f9bede8588740cb2393397bdee56c
entitybtw <studenov.ivan020312@gmail.com>
2026-02-11 09:33:28 +0300

    add intractive zone continue [very unstable, W.I.P]

--- assets/video/episode1/choices/6/the_woods_zone.lua
+++ assets/video/episode1/choices/6/the_woods_zone.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9ee38623201c6560e1ce4c58c83f8a91dfe4303a5f9a42a55feee56b9392acc4
-size 3847
+oid sha256:b445a501794e74cf40dca1ecd2d98a18ffaf9d96d718d446e51a47fbec755406
+size 4136
--- assets/video/episode1/start.lua
+++ assets/video/episode1/start.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:14608e371fca464c6b817ca66d76f38e076028b0a517d6ffb4ffb9adef8883ec
-size 6336
+oid sha256:769045994f20f81614f3741dde95b26ec0663222db22b96cf7da13d2cdb0e955
+size 7244
