2d55da9dab491febeb8860ae6c842989ac019ab6
entitybtw <studenov.ivan020312@gmail.com>
2026-05-07 13:44:07 +0300

    rename support menu to stuff

---
+++ assets/misc/stuff.lua
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:50b3932d2dade2b9961bc76506911e19cdd07b5b3d6346e709da0e37cfa4760b
+size 4113
--- assets/misc/support.lua
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1ada966d036419bd61d58f443a90097adba4c8c580d6f2d16adfd205d9b686b6
-size 4115
