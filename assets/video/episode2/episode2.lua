de24ad1a7d66e1dde919c222856475d5e1b34bbb
entitybtw <studenov.ivan020312@gmail.com>
2026-05-31 12:42:09 +0300

    add pmp-play fixes

--- assets/video/episode2/episode2.lua
+++ assets/video/episode2/episode2.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a997a42b69440e884dfb107babb55f11ed790b5232cbbe7d1bcd6d7b491305d
-size 26433
+oid sha256:3c58a2934e012d9e5877b6ea239a1e830588ca02eb1bcaa01df448246c2b9122
+size 26493
