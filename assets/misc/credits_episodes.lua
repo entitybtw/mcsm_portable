848d850b19cb822222c769d525eeaaca7df25eef
entitybtw <studenov.ivan020312@gmail.com>
2026-05-31 12:43:06 +0300

    Revert "add pmp-play fixes"
    This reverts commit f5010ae0f0652b476e9f98307053c1766aa616ac.

--- assets/misc/credits_episodes.lua
+++ assets/misc/credits_episodes.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:120f8e1465c43f8f61966a0f1ba31c0aeef82f1b4a8bed6150f27dd6e62dcb26
-size 5182
+oid sha256:f02ef7c0c75bba69ec2946cca84c4c7f0c9532b55fec4c640f7166966129706d
+size 5107
--- assets/ui/epmenu/epmenu.lua
+++ assets/ui/epmenu/epmenu.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2d3eabff5af52cae7def3158b330c64c65972ac71f8b252947feba5748c9bcf0
-size 5142
+oid sha256:5d6c6ecd4f91772442e34e3a7978da1a3afe8fadc44b56317f0f3aa9b5688377
+size 5007
